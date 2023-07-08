e5c16c03acb7f74d54d5a52dca141e5ae5844121
m.s.jain <m.s.jain@accenture.com>
2023-07-08 18:21:09 +0530

    ngFor Example

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,20 +5,26 @@ import { Component ,EventEmitter,Input, Output} from "@angular/core";
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{event?.price}}</div>
         <div>
-            <span>Location: {{event.location.address}}</span>
+            <span>Location: {{event?.location.address}}</span>
             <span class=pad-left></span>
             <!-- Interpolation -->
-            <span>{{event.location.city}}, {{event.location.country}}</span>
+            <span>{{event?.location.city}}, {{event?.location.country}}</span>
+        </div>
+        <div>
+            Online URL : {{event?.onlineUrl}}
         </div>
     </div>
 
     `,
     styles:[`
+        .thumbnail{
+            min-height:210px;
+        }
         .pad-left {
             margin-left: 10px;
         }
