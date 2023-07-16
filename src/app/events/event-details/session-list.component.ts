ebd68ccff070387d732d96579dd775c9f563d5e4
m.s.jain <m.s.jain@accenture.com>
2023-07-16 18:00:38 +0530

    auth made public so that it can be accessible

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -14,7 +14,7 @@ export class SessionListComponent implements OnChanges{
     @Input() filterBy:string;
     @Input() sortBy:string;
     visibleSessions : ISession[]=[];
-    constructor(private auth:AuthService ,private voterService:VoterService){
+    constructor(public auth:AuthService ,private voterService:VoterService){
 
     }
     ngOnChanges(changes: SimpleChanges): void {
