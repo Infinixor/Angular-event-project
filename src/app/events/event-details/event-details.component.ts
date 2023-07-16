4aff07e001f8d7fe1e590699d3ffd33fd2222c33
m.s.jain <m.s.jain@accenture.com>
2023-07-16 11:40:39 +0530

    sorting function

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -20,7 +20,7 @@ export class EventDetailsComponent implements OnInit {
     addMode: boolean;
 
     filterBy:string = 'all';
-    
+    sortBy:string = 'votes';
     constructor(private eventService:EventService , private route : ActivatedRoute){
 
     }
